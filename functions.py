afbdd87bc901dc399c737e0f42cb35824c9dd0cc
Damir <zagidullin.damirka@mail.ru>
2024-10-11 16:03:12 +0500

    2. Реализация функции AddNewAction и добавление функции GetActionsOnDay

--- functions.py
+++ functions.py
@@ -1,7 +1,13 @@
-def AddNewAction():
-    pass
+from main import todoList
+
+
+def AddNewAction(day: int, action):
+    todoList[day] = action
 
 def GetAllActions():
+    pass
+
+def GetActionsOnDay(day: int):
     pass
 
 
