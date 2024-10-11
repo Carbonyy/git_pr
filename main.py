71068e5d4a77b0e13ea97dd122718e108f49ad3e
Damir <zagidullin.damirka@mail.ru>
2024-10-11 16:19:48 +0500

    5. Перенос функций в 1 файл и удаление functions.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import functions
 
 
-todoList = {}
+def AddNewAction(day: int, action: str):
+    todoList[day] = action
+
+def GetAllActions():
+    for i in range(todoList.keys()):
+        print(todoList.get(i))
+        print('\n')
+
+def GetActionsOnDay(day: str):
+    print(todoList.get(day))
+
+
+
+todoList = dict()
+AddNewAction('1', "побегать")
+GetActionsOnDay('1')
